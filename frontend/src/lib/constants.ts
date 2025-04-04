e272ad518cededd21913d0aaa14ffed2b7d9146f
calvin <calvin71203@gmail.com>
2025-04-04 02:29:09 -0400

    ih8this

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,8 +1,7 @@
 // Base api url
 //export const BASE_URL = "http://localhost:8080/api";
 
-const BASE = process.env.NEXT_PUBLIC_API_URL;
-export const BASE_URL = `${BASE}/api`;
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Token
 export const TOKEN_KEY = "gametrack_token";
