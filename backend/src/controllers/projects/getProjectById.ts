c52d6de095cf2f11b38d57cd5b9ba33a527c2d3b
calvin <calvin71203@gmail.com>
2025-04-04 17:31:24 -0400

    Cant access uninvited projects

--- backend/src/controllers/projects/getProjectById.ts
+++ backend/src/controllers/projects/getProjectById.ts
@@ -1,5 +1,32 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const getProjectById = async (req: Request, res: Response) => {};
+const getProjectById = async (req: Request, res: Response) => {
+  const projectId = parseInt(req.params.id);
+  const userId = (req as any).user;
+
+  try {
+    const findProject = await prisma.project.findFirst({
+      where: {
+        id: projectId,
+        members: {
+          some: {
+            userId: userId,
+          },
+        },
+      },
+    });
+
+    if (!findProject) {
+      res.status(403).json({ error: "Access denied" });
+      return;
+    }
+
+    res.json({ findProject });
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
 
 export default getProjectById;
