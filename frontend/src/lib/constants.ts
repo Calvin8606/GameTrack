7ed0863873acbeed2754677e02a0dfe0ee566586
calvin <calvin71203@gmail.com>
2025-04-03 21:01:11 -0400

    constants

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,5 +1,6 @@
 // Base api url
-export const BASE_URL = "http://localhost:8080/api";
+//export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://www.gametrack.dev/api";
 
 // Token
 export const TOKEN_KEY = "gametrack_token";
